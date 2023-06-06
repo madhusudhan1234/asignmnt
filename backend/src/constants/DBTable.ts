48f8aa8456a55d981fc337d2ee590dfbbac1d561
Mac <mac@Macs-MacBook-Pro.local>
2023-06-06 21:23:30 +0545

    Seperating Frontend

--- backend/src/constants/DBTable.ts
+++
@@ -1,7 +0,0 @@
-export const IMAGES = "images";
-export const CATEGORIES = "categories";
-export const SUBCATEGORIES = "subcategories";
-export const PRODUCTS = "products";
-export const IMAGE_PRODUCT = "product_image";
-export const SUBSCRIBERS = "subscribers";
-export const USERS = "users";
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = "down" ]; then
-  cd frontend && docker-compose down
-  cd ../backend && docker-compose down
-else
-  cd frontend && docker-compose up -d
-  cd ../backend && docker-compose up 
-fi
